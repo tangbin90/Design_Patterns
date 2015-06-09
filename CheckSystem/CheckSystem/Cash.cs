14dfeefa03f170bb25cec2acba660106700a6ec2
tangbin <tangbin90@gmail.com>
2015-06-09 23:40:01 +0800

    Chapter 2.1 homework

---
+++ CheckSystem/CheckSystem/Cash.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckSystem
+{
+    abstract class CashBase
+    {
+        public abstract double acceptCash(double money);
+    }
+
+    class CashNormal : CashBase
+    {
+        public override double acceptCash(double money)
+        {
+ 	        return money;
+        }
+    }
+
+    class CashCount :CashBase
+    {
+        private double _discount=1;
+        public double discount
+        {
+            get { return _discount; }
+            set{
+                if(value<=1&&value>0)
+                {
+                    _discount=value;
+                }
+               }
+         }
+        public CashCount(string count)
+        {
+            discount=double.Parse(count);
+        }
+        public override double acceptCash(double money)
+        {
+            return money * discount;
+         }
+    }
+
+    class CashReturn : CashBase
+    {
+        private double moneyCondition = 0.0d;
+        private double moneyReturn = 0.0d;
+        public CashReturn(string moneyCondition, string moneyReturn)
+        {
+            this.moneyCondition = double.Parse(moneyCondition);
+            this.moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double acceptCash(double money)
+        {
+            double result = money;
+            if(money >=moneyCondition)
+            { 
+                result = money - Math.Floor(money / moneyCondition) * moneyReturn;
+            }
+            return result;
+        }
+    }
+
+    class CashFactory
+    {
+        public static CashBase creatCashAccept(string type)
+        {
+            CashBase cs = null;
+            switch(type)
+            {
+                case "正常收费":
+                    cs=new CashNormal();
+                    break;
+                case "满300反100":
+                    CashReturn crl = new CashReturn("300","100");
+                    cs=crl;
+                    break;
+                case "打八折":
+                    CashCount cr2 = new CashCount("0.8");
+                    cs = cr2;
+                    break;
+            }
+            return cs;
+        }
+
+    }
+}
---
+++ CheckSystem/CheckSystem/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckSystem
+{
+    public partial class Form1 : Form
+    {
+        public double total;
+        public Form1()
+        {
+            InitializeComponent();
+            calway.Items.AddRange(new object[] { "正常收费", "打八折", "满300反100"});
+            calway.SelectedIndex = 0;
+            label5.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double totalPrices = 0d;
+            CashBase cashbase = CashFactory.creatCashAccept(calway.SelectedItem.ToString());
+            totalPrices = cashbase.acceptCash(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+
+            total = total + totalPrices;
+            listBox1.Items.Add("单价: " + textBox1.Text + "数量: " + textBox2.Text + " " + calway.SelectedItem + " 合计： " + totalPrices.ToString());
+            label5.Text=total.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            label5.Text = "";
+            total = 0;
+            calway.SelectedIndex = 0;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
